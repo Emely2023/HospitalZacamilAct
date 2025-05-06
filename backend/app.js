4f9b7848ad50d045c6ae7d215a9db4861acc875f
emely <20230621@ricaldone.edu.sv>
2025-05-06 18:01:58 -0300

    installed all and code

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+// Importo todo lo de la libreria de Express
+import express from "express";
+
+// Creo una constante que es igual a la libreria que importé
+const app = express();
+
+//Que acepte datos en json
+app.use(express.json());
+//Que postman acepte guardar cookies
+app.use(cookieParser());
+
+// Definir las rutas de las funciones que tendrá la página web
+
+
+
+
+// Exporto la constante para poder usar express en otros archivos
+export default app;
---
+++ backend/src/config.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+
+// Ejecuto "Dotenv"
+dotenv.config();
+
+export const config = {
+    db:{
+        URI: process.env.DB_URI || "mongodb://localhost:27017/ZonaDigitalDB20230621",
+    },
+    server:{
+        port: process.env.PORT || 4000,
+    },
+
+};
